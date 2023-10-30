75a683f4783f576f54fd8beafd6d49f6a3806be1
Vladislavs.Viktorovs <vv.viktorovs@gmail.com>
2023-10-30 08:46:21 +0100

    Introduce SignalActionFactory interface
    - Depending on the business logic and requirements this factory may be implemented in various ways

---
+++ src/signal/SignalActionFactory.java
@@ -0,0 +1,10 @@
+package signal;
+
+public interface SignalActionFactory {
+    /**
+     * Creates a [SignalAction] based on the specified value.
+     * @param value the value of a signal to create a [SignalAction] for.
+     * @return [SignalAction] for a provided signal value
+     * */
+    SignalAction create(int value);
+}
