92e8c046da4ab3ba8e0e56dcf79faf81e556e421
Voltorane <vv.viktorovs@gmail.com>
2023-11-01 10:42:41 +0100

    Add mock testing suite
    - For each signal implemented, tests are provided, that check calls to the upcall libraries

--- src/main/java/com/db/signaltrade/ApplicationTest.java
+++ src/main/java/com/db/signaltrade/ApplicationTest.java
@@ -1,5 +1,9 @@
 package com.db.signaltrade;
 
+import com.db.signaltrade.action.DefaultSignalAction;
+import com.db.signaltrade.action.SignalAction;
+import com.db.signaltrade.action.SignalActionFactory;
+import com.db.signaltrade.action.SignalActionFactoryImpl;
 import com.othercompany.lib.Algo;
 import com.othercompany.lib.SignalHandler;
 import org.junit.jupiter.api.Test;
@@ -8,8 +12,16 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import org.mockito.Mockito;
 
 class ApplicationTest {
+    private final SignalActionFactory signalActionFactory = new SignalActionFactoryImpl();
+    private final Algo algoMock = Mockito.mock(Algo.class);
+
     // Legacy application that should be replaced by the new application.
     private static class LegacyApplication implements SignalHandler {
         public static int signalsImplemented = 3;
@@ -62,5 +74,51 @@ class ApplicationTest {
 
         // compare the output of the two applications
         assertEquals(legacyBuffer.toString(), appBuffer.toString());
+    }
+
+    @Test
+    void testDefaultAction() {
+        SignalAction action = new DefaultSignalAction();
+        action.perform(algoMock);
+
+        verify(algoMock).cancelTrades();
+
+        verify(algoMock, never()).setUp();
+        verify(algoMock, never()).reverse();
+        verify(algoMock, never()).setAlgoParam(anyInt(), anyInt());
+        verify(algoMock, never()).performCalc();
+        verify(algoMock, never()).submitToMarket();
+    }
+
+    @Test
+    void testSignalAction_1() {
+        SignalAction action = signalActionFactory.create(1);
+        action.perform(algoMock);
+
+        verify(algoMock).setUp();
+        verify(algoMock).setAlgoParam(1, 60);
+        verify(algoMock).performCalc();
+        verify(algoMock).submitToMarket();
+    }
+
+    @Test
+    void testSignalAction_2() {
+        SignalAction action = signalActionFactory.create(2);
+        action.perform(algoMock);
+
+        verify(algoMock).reverse();
+        verify(algoMock).setAlgoParam(1, 80);
+        verify(algoMock).submitToMarket();
+    }
+
+    @Test
+    void testSignalAction_3() {
+        SignalAction action = signalActionFactory.create(3);
+        action.perform(algoMock);
+
+        verify(algoMock).setAlgoParam(1, 90);
+        verify(algoMock).setAlgoParam(2, 15);
+        verify(algoMock).performCalc();
+        verify(algoMock).submitToMarket();
     }
 }
